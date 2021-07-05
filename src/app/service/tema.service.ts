32c154b444f2c92c83f6fce9aa5d4403f09fd99d
Danilo Silva <danilo.dos.s.silva@gmail.com>
2021-07-05 11:02:58 -0300

    Put e Delete implementados com sucesso

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -24,7 +24,19 @@ export class TemaService {
       return this.http.get<Tema[]>('https://backendthiagofaccipieri.herokuapp.com/tema', this.token)      
     }
 
+    getByIdTema(id:number):Observable<Tema>{
+      return this.http.get<Tema>(`https://backendthiagofaccipieri.herokuapp.com/tema/${id}`, this.token)
+    }
+
     postTema(tema: Tema): Observable<Tema>{
       return this.http.post<Tema>('https://backendthiagofaccipieri.herokuapp.com/tema', tema, this.token)
+    }
+
+    putTema(tema: Tema): Observable<Tema>{
+      return this.http.put<Tema>('https://backendthiagofaccipieri.herokuapp.com/tema', tema, this.token)
+    }
+
+    deleteTema(id: number){
+      return this.http.delete(`https://backendthiagofaccipieri.herokuapp.com/tema/${id}`, this.token)
     }
 }
